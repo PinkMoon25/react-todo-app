df3917bacf38cb7110a3c17a19324e87b38069fb
Tushar <singhtushar348@gmail.com>
2022-08-09 21:48:38 +0100

    Add Routing with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter,Route, Routes } from 'react-router-dom';
 import './App.css';
 import TodoContainer from './components/TodoContainer';
+import Navbar from './components/Navbar';
+import About from './pages/about';
 
 function App() {
   return (
-    <TodoContainer />
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+    <Route path="/" element={<TodoContainer />}>
+      {/* <TodoContainer /> */}
+    </Route>
+    <Route path="/about" element={<About />}>
+      {/* <About /> */}
+    </Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++
@@ -1,7 +0,0 @@
-const Header = () => (
-  <header>
-    <h1>To-D0:</h1>
-  </header>
-);
-
-export default Header;
